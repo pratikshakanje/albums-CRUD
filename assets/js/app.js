3bec10b0fb083d514706913b1d45416e0728198a
pratikshalifeshield <pratiksha.lifeshield@gmail.com>
2026-09-12 21:52:18 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,40 @@
+const cl = console.log;
+
+const BASE_URL = "https://jsonplaceholder.typicode.com"
+
+const ALBUMS_URL = `${BASE_URL}/albums`
+
+let xhr = new XMLHttpRequest()
+
+xhr.open("GET", ALBUMS_URL)
+
+xhr.onload = function () {
+    if (xhr.status === 200) {
+        let data = JSON.parse(xhr.response)
+        console.log(data);
+
+        let result = ``
+
+        data.forEach(albums => {
+            result += ` <div class="col-4 mt-4" id="${albums.id}">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h4>${albums.title}</h4>
+                    </div>
+                    <div class="card-footer d-flex justify-content-between">
+                        <button class="btn btn-sm btn-outline-primary">Add</button>
+                        <button class="btn btn-sm btn-outline-danger">Delete</button>
+                    </div>
+                </div>
+            </div>`
+
+
+            const albumsContainer = document.getElementById("albumsContainer")
+            albumsContainer.innerHTML = result
+        });
+    } else {
+        cl("error")
+    }
+}
+
+xhr.send()
